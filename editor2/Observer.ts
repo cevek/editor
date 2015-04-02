130dce9127f8e911e729d153247a16b3c43c8f00
cody <cevek@ya.ru>
2015-04-02 11:45:55 +0300

    observer refactoring

--- editor2/Observer.ts
+++ editor2/Observer.ts
@@ -1,18 +1,32 @@
 var __observe_stack:any[] = [];
+
+var Sym = (<any>window).Symbol || function (name:string) {return '__' + name};
+var globalObserver = Sym("observer");
+var globalListeners = Sym("listeners");
+
 function observe(obj:any, key:string) {
     return {
+        enumerable: true,
         get: function () {
             __observe_stack.push([this, key]);
-            return this[key + "_"];
+            if (!this[globalObserver]) {
+                this[globalObserver] = {};
+                this[globalObserver][globalListeners] = {};
+            }
+            return this[globalObserver][key];
         },
         set: function (val:any) {
-            if (this.__listeners && this.__listeners[key]) {
-                for (var i = 0; i < this.__listeners[key].length; i++) {
-                    var listener = this.__listeners[key][i];
+            if (!this[globalObserver]) {
+                this[globalObserver] = {};
+                this[globalObserver][globalListeners] = {};
+            }
+            if (this[globalObserver][globalListeners][key]) {
+                for (var i = 0; i < this[globalObserver][globalListeners][key].length; i++) {
+                    var listener = this[globalObserver][globalListeners][key][i];
                     listener();
                 }
             }
-            this[key + "_"] = val;
+            this[globalObserver][key] = val;
         }
     }
 }
@@ -35,9 +49,9 @@ Object.defineProperty(window, 'Observer', {
                 listen: function (f:()=>any) {
                     for (var i = 0; i < stack.length; i++) {
                         var item = stack[i];
-                        item[0].__listeners = item[0].__listeners || {};
-                        item[0].__listeners[item[1]] = item[0].__listeners[item[1]] || [];
-                        item[0].__listeners[item[1]].push(f);
+                        var listeners = item[0][globalObserver][globalListeners];
+                        listeners[item[1]] = listeners[item[1]] || [];
+                        listeners[item[1]].push(f);
                     }
                     console.assert(stack.length === args.length, `Not all variables observed: ${stack.length} of ${args.length}`, stack);
                 }
@@ -55,7 +69,8 @@ class Test {
 var test = new Test();
 
 class CCC {
-    index: number = 1;
+    index:number = 1;
+
     constructor() {
         Observer(test.a, test.b).listen(()=> {
             console.log("observer launch", this.index);
