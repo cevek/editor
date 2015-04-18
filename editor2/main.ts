6c1ef28170904db534eeebb07250d184737e9905
cody <cevek@ya.ru>
2015-04-18 19:54:09 +0300

    small fixes

--- editor2/main.ts
+++ editor2/main.ts
@@ -6,7 +6,7 @@
 ///<reference path="Store.ts"/>
 ///<reference path="ReactDOM.ts"/>
 ///<reference path="HistoryService.ts"/>
-///<reference path="editor.ts"/>
+///<reference path="LinesStore.ts"/>
 ///<reference path="KeyboardKey.ts"/>
 ///<reference path="editor/EditorView.ts"/>
 ///<reference path="test/LinesStore.spec.ts"/>
@@ -14,13 +14,19 @@
 ///<reference path="../typings/webaudioapi/waa.d.ts"/>
 
 
-function copy(obj:any, to:any) {
-    if (obj) {
-        var keys = Object.keys(obj);
-        for (var i = 0; i < keys.length; i++) {
-            var key = keys[i];
-            to[key] = obj[key];
-        }
-    }
-    return to;
-}
+var linesStore = new LinesStore();
+Promise.all([
+    HTTP.get<string>('../data/enSub.srt', true),
+    HTTP.get<string>('../data/ruSub.srt', true)
+]).then((values)=> {
+    console.log(values);
+
+    linesStore.parse(values[0], values[1]);
+    React.render(React.createElement(editor.EditorView), document.getElementById('body'));
+});
+
+HTTP.get<ArrayBuffer>('../data/enAudio.mp3', true, 'arraybuffer')
+    .then((data)=>
+        new Promise<AudioBuffer>((resolve, reject)=>
+            new AudioContext().decodeAudioData(data, resolve, reject)))
+    .then((data)=>linesStore.audioData = data);
